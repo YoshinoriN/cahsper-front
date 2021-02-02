a741cea6cec106379d14194f7306aa4f717f0794
YoshinoriN <nayo-msoft@outlook.jp>
2021-02-02 23:24:26 +0900

    add config.js

---
+++ config.example.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'apiUrl': 'api.example.com',
+  'defaultSortType': 'desc', // desc, asc
+  'defaultDateFrom': '2020-01-01', //YYYY-MM-DD
+  'defaultCurrentPage': 1,
+  'defaultPerPage': 30
+}
