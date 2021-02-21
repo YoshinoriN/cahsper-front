770aa71c0e125c389fdfd6e08e0dcb2ee1038a8f
yoshinorin <yoshinorin.net@outlook.com>
2021-02-22 00:12:01 +0900

    add font from CDN

--- components/headerMeta.tsx
+++ components/headerMeta.tsx
@@ -4,6 +4,7 @@ export default function HeaderMeta() {
   return (
     <Head>
       <meta name="robots" content="noindex, noarchive, noimageindex, nofollow"></meta>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
       {/* TODO: add title,  */}
     </Head>
   )
