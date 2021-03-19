37dfd0df0a6dad93c46cbe6b671eecf138796aad
yoshinorin <yoshinorin.net@outlook.com>
2021-03-19 23:14:01 +0900

    feat: footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,17 @@
+import { Container } from '@material-ui/core';
+import { Button } from '@material-ui/core'
+
 export default function Footer() {
   return (
-    <footer>
-      {/* TODO: add something,  */}
-    </footer>
+    <Container maxWidth="md">
+      <footer>
+        <div>
+          <hr/>
+          <Button href="https://github.com/yoshinorin/cahsper" color="primary" style={{textTransform: 'none'}}>
+            Powerd by Cahpser
+          </Button>
+        </div>
+      </footer>
+    </Container>
   )
 }
