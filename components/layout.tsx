452f6fd1ce9f35d36ec47487278a611b3c01e1d8
yoshinorin <yoshinorin.net@outlook.com>
2021-03-19 22:42:02 +0900

    feat: always display appbar and footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,9 @@
 import Footer from '../components/footer'
-import HeaderMeta from '../components/headerMeta'
 import AppBar from '../components/appBar';
 
 export default function Layout({children}) {
   return (
     <>
-      <HeaderMeta />
       <AppBar />
       <div>
         <main>{children}</main>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,15 @@
+import Layout from '../components/layout'
+import HeaderMeta from '../components/headerMeta'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <HeaderMeta />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
 
 export default MyApp
